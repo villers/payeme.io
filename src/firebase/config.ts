9170d85b9fe90031fabdb0a41129cc7f8f5d9125
VILLERS Mickaël <mickael.villers@epitech.eu>
2022-09-10 09:32:39 +0200

    chore: install prettier

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import {initializeApp} from "firebase/app";
-import {connectAuthEmulator, getAuth} from "firebase/auth";
-import {connectDatabaseEmulator, getDatabase} from "firebase/database";
+import { initializeApp } from "firebase/app";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
+import { connectDatabaseEmulator, getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,8 +19,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app)
-export const db = getDatabase(app)
+export const auth = getAuth(app);
+export const db = getDatabase(app);
 
 if (location.hostname === "localhost") {
   connectDatabaseEmulator(db, "localhost", 9000);
